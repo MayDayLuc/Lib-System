d05bc1465c4c47c3f5e1d9099d3d0e3ea8ab7239
LuckY_ <34900270@qq.com>
2019-01-21 17:48:36 +0800

    Merge branch 'master' of github.com:MayDayLuc/Lib-System
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Library System");
+        primaryStage.setScene(new Scene(root, 1200, 742));
         primaryStage.show();
     }
 
