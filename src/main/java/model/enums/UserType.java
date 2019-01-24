5d40b6558d65163cffd70539b235d1846f714c65
LuckY_ <34900270@qq.com>
2019-01-24 13:57:19 +0800

    user change Info

--- src/main/java/model/enums/UserType.java
+++ src/main/java/model/enums/UserType.java
@@ -1,5 +1,15 @@
 package model.enums;
 
 public enum UserType {
-    TEACHER, UNDERGRADUATE, GRADUATE, ADMINISTRATOR
+    TEACHER, UNDERGRADUATE, GRADUATE, ADMINISTRATOR;
+
+    public static String transform(UserType userType){
+        switch (userType){
+            case UNDERGRADUATE: return "本科生";
+            case TEACHER: return "教师";
+            case ADMINISTRATOR: return "管理员";
+            case GRADUATE: return "研究生";
+            default:return "";
+        }
+    }
 }
