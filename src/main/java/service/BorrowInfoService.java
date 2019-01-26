768736e3f1e4cbe5425f045d53e711110cc7a2c4
LuckY_ <34900270@qq.com>
2019-01-26 22:27:08 +0800

    Origin searchfield on user, book and borrowInfo

--- src/main/java/service/BorrowInfoService.java
+++ src/main/java/service/BorrowInfoService.java
@@ -9,4 +9,5 @@ public interface BorrowInfoService {
     public List<Book> getMyBorrowedBooks(String uid);
     public int getBorrowedCnt(String uid);
     public List<BorrowInfo> getAllBorrowedBooks();
+    public List<BorrowInfo> getKeyBorrow(String key);
 }
